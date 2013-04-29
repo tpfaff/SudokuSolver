1be72982f6d5ac98f3e699acaa3601af2f0d94f8
unknown <tylerpfaff@gmail.com>
2013-04-29 16:18:27 -0700

    Initial commit

---
+++ src/Cell.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+
+
+public class Cell {
+	private int row,column;
+	private int value;
+	private ArrayList<Integer> invalidOptions=new ArrayList<Integer>();
+	
+	
+	public Cell(int row,int column, int value)
+	{
+		this.row=row;
+		this.column=column;
+		this.value=value;
+		
+	}
+	
+	public Cell() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getValue(){
+		return value;
+	}
+	public void setValue(int value){
+		this.value=value;
+	}
+	public void setCoordinates(int row,int column){
+		this.row=row;
+		this.column=column;
+	}
+	public void setInvalidOptions(ArrayList<Integer> invalidOptions){
+			this.invalidOptions=invalidOptions;
+
+	}
+	public ArrayList<Integer> getInvalidOptions(){
+		return invalidOptions;
+	}
+	}
+
